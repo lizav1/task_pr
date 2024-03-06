f2da35ce0ff1de5774996b1532756574b99956f2
Liza F <o27681119@gmail.com>
2024-03-06 16:18:44 +0200

    create app, add models

---
+++ taskapp/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+# class user, implement logic
+# class User
+
+class TaskTag(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class TaskStatus(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Task(models.Model):
+    creator = models.ForeignKey(User, related_name="creator", on_delete=models.SET_NULL, null=True)
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    status = models.ForeignKey(TaskStatus, on_delete=models.SET_NULL, null=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    assignee = models.ForeignKey(User,related_name="assignee", on_delete=models.SET_NULL, null=True)
+
---
+++ taskapp/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def projectView(request):
+    return HttpResponse("OK!")
+
+
+# 
+def taskView():
+    pass
+
+
