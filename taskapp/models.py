bf30fac7566351814b80ed3050bcfc182909194c
Liza F <o27681119@gmail.com>
2024-03-08 00:46:19 +0200

    -adding edit attribute to a model

--- taskapp/models.py
+++ taskapp/models.py
@@ -13,6 +13,10 @@ class TaskStatus(models.Model):
     name = models.CharField(max_length=255)
     description = models.CharField(max_length=255, blank=True)
 
+    def __str__(self):
+        return self.name
+
+
 
 class Task(models.Model):
     creator = models.ForeignKey(User, related_name="creator", on_delete=models.SET_NULL, null=True)
@@ -22,3 +26,7 @@ class Task(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     assignee = models.ForeignKey(User,related_name="assignee", on_delete=models.SET_NULL, null=True)
+    edited = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ["-created"]
