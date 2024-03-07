7e8683fb193ea8098e30802a4058c7dad1214c3c
Liza F <o27681119@gmail.com>
2024-03-07 14:19:05 +0200

    -add gitignore -provide logic of creating, deleting, editing,viewing task -customize template, views, urls for task model

--- taskapp/urls.py
+++ taskapp/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from . import views
+from .views import home_view,create_task_view, edit_task_view, delete_task, view_task
 
 urlpatterns = [
-    path('', views.projectView, name='project')
+    path('', home_view, name='home'),
+    path('create_task', create_task_view, name='create_task'),
+    path('edit_task/<int:id>/', edit_task_view, name='edit_task'),
+    path('delete_task/<int:id>/', delete_task, name='delete_task'),
+    path('task/<int:id>/', view_task, name='task')
 ]
--- taskapp/views.py
+++ taskapp/views.py
@@ -1,14 +1,61 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Task
-from django.views.generic.detail import DetailView
+from .forms import TaskForm
 
-def homeView(request):
-    model = Task
+
+def home_view(request):
+    """
+    show all tasks for current user
+
+    """
     tasks = Task.objects.all()
     context = {'tasks': tasks}
     return render(request, 'taskapp/home.html', context=context)
 
 
-class taskView(DetailView):
-    model = Task
-    template_name = 'taskapp/task.html'
+def create_task_view(request):
+    """
+    create task from form
+    """
+    if request.method == "POST":
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    form = TaskForm()
+    context = {'form': form}
+    return render(request, 'taskapp/create_task.html', context=context)
+
+
+def edit_task_view(request, id):
+    """
+    update current task
+    """
+    task = Task.objects.get(id=id)
+    if request.method == "POST":
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            redirect('home')
+    form = TaskForm()
+    context = {'form': form}
+    return render(request,'taskapp/edit_task.html')
+
+
+def delete_task(request, id):
+    """
+    delete task
+    """
+    task = Task.objects.get(id=id)
+    task.delete()
+    return redirect('home')
+
+
+def view_task(request, id):
+    """
+    view task
+    """
+    task = Task.objects.get(id=id)
+    context = {'task': task}
+    return render(request, 'taskapp/task.html', context=context)
+
